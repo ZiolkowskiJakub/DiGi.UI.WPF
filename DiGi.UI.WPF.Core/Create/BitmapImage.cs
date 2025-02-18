3409d74f618503d438d357025e1f7dd8a586014b
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-02-18 08:46:02 +0100

    Create BitmapImage from bytes implemented

---
+++ DiGi.UI.WPF.Core/Create/BitmapImage.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace DiGi.UI.WPF.Core
+{
+    public static partial class Create
+    {
+        public static BitmapImage BitmapImage(this byte[] bytes)
+        {
+            if(bytes == null)
+            {
+                return null;
+            }
+
+            using (MemoryStream memoryStream = new MemoryStream(bytes))
+            {
+                BitmapImage result = new BitmapImage();
+                result.BeginInit();
+                result.CacheOption = BitmapCacheOption.OnLoad;
+                result.StreamSource = memoryStream;
+                result.EndInit();
+                result.Freeze();
+                return result;
+            }
+        }
+    }
+}
