8a48de13fcb334c72f6fccb53ce15b76420283cc
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-02-19 01:09:16 +0100

    Path and Paths query implemented

---
+++ DiGi.UI.WPF.Core/Query/Path.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+
+namespace DiGi.UI.WPF.Core
+{
+    public static partial class Query
+    {
+        public static string? Path(this System.Windows.Window window, string fileTypeName, string fileExtension)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Title = string.Format("Select {0}", fileTypeName),
+                Filter = string.Format("{0} (*.{1})|*.{1}|All Files (*.*)|*.*", fileTypeName, fileExtension),
+                FilterIndex = 1,
+                Multiselect = false
+            };
+
+            bool? result = openFileDialog.ShowDialog(window);
+            if (result == null || !result.HasValue || !result.Value)
+            {
+                return null;
+            }
+
+            return openFileDialog.FileName;
+        }
+    }
+}
