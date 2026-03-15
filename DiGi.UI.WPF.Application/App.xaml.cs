91e5642e049e866feed26204e4bf62353d2a9626
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-03-15 07:05:19 +0100

    DiGi.UI.WPF.Core renamed to DiGi.UI.WPF

--- DiGi.UI.WPF.Application/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DiGi.UI.WPF.Application
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : System.Windows.Application
-    {
-    }
-}
